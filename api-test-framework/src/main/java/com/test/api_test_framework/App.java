11aad0694cdb1b8c9dad5ef12fface3e8419a7c9
Liza <ljena@adobe.com>
2022-08-26 12:37:58 +0530

    new project setup

---
+++ api-test-framework/src/main/java/com/test/api_test_framework/App.java
@@ -0,0 +1,13 @@
+package com.test.api_test_framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
